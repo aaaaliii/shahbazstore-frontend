842e1c6bd39499acd9e1e71188a555d637d4eeba
afaq-bin-naeem <afaqbinnaeem@gmail.com>
2026-03-09 20:33:54 +0500

    updated responsive product carousel

--- components/product/ProductCarousel.tsx
+++ components/product/ProductCarousel.tsx
@@ -1,9 +1,12 @@
-'use client';
+"use client";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Product } from '@/types';
-import { ProductCard } from './ProductCard';
-import 'swiper/css';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+import { Product } from "@/types";
+import { ProductCard } from "./ProductCard";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 interface ProductCarouselProps {
   products: Product[];
@@ -11,35 +14,43 @@ interface ProductCarouselProps {
 
 export function ProductCarousel({ products }: ProductCarouselProps) {
   return (
-    <Swiper
-      spaceBetween={16}
-      slidesPerView={1}
-      breakpoints={{
-        576: {
-          slidesPerView: 2,
-          spaceBetween: 20,
-        },
-        768: {
-          slidesPerView: 3,
-          spaceBetween: 24,
-        },
-        992: {
-          slidesPerView: 4,
-          spaceBetween: 24,
-        },
-        1200: {
-          slidesPerView: 5,
-          spaceBetween: 28,
-        },
-      }}
-      className="products-slider"
-    >
-      {products.map((product) => (
-        <SwiperSlide key={product.id}>
-          <ProductCard product={product} />
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <div className="products-slider-wrapper">
+      <Swiper
+        modules={[Navigation, Pagination]}
+        spaceBetween={16}
+        slidesPerView={1}
+        breakpoints={{
+          400: {
+            slidesPerView: 1.5,
+            spaceBetween: 16,
+          },
+          576: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 24,
+          },
+          992: {
+            slidesPerView: 4,
+            spaceBetween: 24,
+          },
+          1300: {
+            slidesPerView: 5,
+            spaceBetween: 28,
+          },
+        }}
+        navigation
+        pagination={{ clickable: true }}
+        className="products-slider"
+      >
+        {products.map((product) => (
+          <SwiperSlide key={product.id}>
+            <ProductCard product={product} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   );
 }
-
